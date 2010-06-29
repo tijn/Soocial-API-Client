1d5715be6cd7c461a52e2646467bafba9f6a6c3a
Tijn Schuurmans <tijn@soocial.com>
2010-06-29 16:57:31 +0200

    removing my own patches for testing the code against a local server

--- examples/common.rb
+++ examples/common.rb
@@ -5,8 +5,3 @@ Dir.glob("#{APP_ROOT}/../lib") { |libdir| $:.unshift libdir }
 require 'rubygems'
 gem 'oauth', '>= 0.3.6'
 require 'oauth'
-
-require 'soocial/basic_client'
-CONSUMER_KEY_ACQUIRED_BY_REGISTERING_YOUR_APP_ON_SOOCIAL = 'G9mX3kZPMOL1AvPUJX8vnQ'
-CONSUMER_SECRET_ACQUIRED_BY_REGISTERING_YOUR_APP_ON_SOOCIAL = 'STJqFWIQwclxWcRJucdvf3hX61YXQLt25fgQrlBHI'
-Soocial::SITE = 'http://localhost:3000'
